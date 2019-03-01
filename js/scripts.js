4bea60d604f03fa60c7b1bf5a63f9a871a23320c
lizKimita <kimita.wanjiku@gmail.com>
2019-03-01 18:37:08 +0300

    adding roll button functionality for player 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,41 @@
 //USER INTERFACE (FRONT-END);
+function RollDice(name, score1, score2, score3) {
+  this.playerName = name;
+  this.dieScore = score1;
+  this.turnScore = score2;
+  this.totalScore = score3;
+}
+
+function rollDice(){
+  return (Math.floor(Math.random()*6)+1);
+}
+
+
+var player1 = "";
+var player2 = "";
+
 $(document).ready(function(){
   $("form#players").submit(function(event){
     event.preventDefault();
     $("#gamebody").slideDown();
+
+    player1 = new RollDice;
+    player2 = new RollDice;
+
+    this.playerName = $("input#player1").val();
+    this.playerName = $("input#player2").val();
+
+    $("#first").text(player1);
+    $("#second").text(player2);
+
+
+  });
+  $("#roll").click(function() {
+    player1 = new RollDice;
+    this.dieScore = rollDice();
+    $("#dieScore1").text(this.dieScore);
   });
 
-
-
-
-
 });
+
+//BUSINESS LOGIC
