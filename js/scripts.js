94958103241c56275c3f1e2c0420afc87bda9f6c
lizKimita <kimita.wanjiku@gmail.com>
2019-03-02 12:13:54 +0300

    adding hold button functionalities in both js and html

--- js/scripts.js
+++ js/scripts.js
@@ -27,15 +27,38 @@ $(document).ready(function(){
   $("#roll1").click(function() {
     this.dieScore = rollDice();
     $("#dieScore1").text(this.dieScore);
-
-    $("#turnScore1").text (this.turnScore);
+    score1(this.dieScore);
 
   });
-
   $("#roll2").click(function(){
     this.dieScore = rollDice();
     $("#dieScore2").text(this.dieScore);
 });
+
+  $("#Hold1").click(function(){
+    $(document.getElementById("Hold1").disabled = true);
+    $(document.getElementById("roll1").disabled = true);
+  });
+
+  $("#Hold2").click(function(){
+    $(document.getElementById("Hold2").disabled = true);
+    $(document.getElementById("roll2").disabled = true);
+  });
 });
 
 //BUSINESS LOGIC
+function rollDice(){
+  return (Math.floor(Math.random()*6)+1);
+};
+
+function score1 (ans) {
+    if (ans === 1) {
+    $("#comment") .text("Oh no! You just rolled a 1, Your total score goes back to zero!");
+    $(document.getElementById("roll1").disabled = true);
+    $("#turnScore1").text(this.dieScore);
+    $("#totalScore1").text(this.dieScore);
+    }
+    else {
+    $("#comment") .text("You rolled " + ans + ". Play again or hold!");
+    }
+  };
