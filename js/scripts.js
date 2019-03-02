034a9d22970dbe35b1ad60dcda3abcd479306f75
lizKimita <kimita.wanjiku@gmail.com>
2019-03-02 09:46:04 +0300

    adding two roll button functionalities for both player 1 and player 2

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,6 @@ function rollDice(){
   return (Math.floor(Math.random()*6)+1);
 }
 
-
-var player1 = "";
-var player2 = "";
-
 $(document).ready(function(){
   $("form#players").submit(function(event){
     event.preventDefault();
@@ -22,20 +18,17 @@ $(document).ready(function(){
     player1 = new RollDice;
     player2 = new RollDice;
 
-    this.playerName = $("input#player1").val();
-    this.playerName = $("input#player2").val();
-
-    $("#first").text(player1);
-    $("#second").text(player2);
-
-
   });
-  $("#roll").click(function() {
-    player1 = new RollDice;
+  $("#roll1").click(function() {
     this.dieScore = rollDice();
     $("#dieScore1").text(this.dieScore);
+
   });
 
+  $("#roll2").click(function(){
+    this.dieScore = rollDice();
+    $("#dieScore2").text(this.dieScore);
+});
 });
 
 //BUSINESS LOGIC
